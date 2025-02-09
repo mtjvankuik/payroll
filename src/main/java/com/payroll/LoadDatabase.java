94fd1f2fa53a2a6e7631f63bad320ec2ed5c11c4
Martin <mtjvankuik@gmail.com>
2025-02-09 13:56:19 +0100

    initial commit

---
+++ src/main/java/com/payroll/LoadDatabase.java
@@ -0,0 +1,21 @@
+package com.payroll;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.CommandLineRunner;
+
+@Configuration
+public class LoadDatabase {
+    
+    private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
+
+    @Bean
+    CommandLineRunner initDatabase(EmployeeRepository repository){
+        return args -> {
+            log.info("Preloading " + repository.save(new Employee("Bilbo Baggins", "Burglar")));
+            log.info("Preloading " + repository.save(new Employee("Frodo Baggins", "Thief")));
+        };
+    }
+}
